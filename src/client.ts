7c25b0ffc89d4e07a61c80a4f6dba7d458081de4
support@ReviveSoft <support@revivesoft.net>
2024-10-21 18:14:56 -0400

    added LabelTron

--- src/client.ts
+++ src/client.ts
@@ -5,11 +5,13 @@ import { TextareaTron } from "./components/textareatron";
 import {SelecTron} from './components/select';
 import TranslateTron from './components/translate/translatetron';
 import {SuggesTron} from './components/suggest';
+import {LabelTron}  from './components/label';
 
 export {
   TextareaTron,
   SelecTron,
   TranslateTron,
-  SuggesTron
+  SuggesTron,
+  LabelTron
 };
 
